0abe723c4d8fcd0c93c1834369dc3cb56615db49
Michael Oyer <code@michaeloyer.com>
2019-09-21 17:33:08 -0400

    Fully Support Email Logging

---
+++ NameSiloDnsUpdateService/EmailLoggingExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using Serilog.Configuration;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Sinks.Email;
+using System;
+using System.Net;
+
+namespace NameSiloDnsUpdateService
+{
+    public static class EmailLoggingExtensions
+    {
+        public static LoggerConfiguration Email(this LoggerSinkConfiguration loggerConfiguration, MyEmailConnectionInfo myConnectionInfo, string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}", LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose, int batchPostingLimit = 100, TimeSpan? period = null, IFormatProvider formatProvider = null, string mailSubject = "Log Email") =>
+            loggerConfiguration.Email((EmailConnectionInfo)myConnectionInfo, outputTemplate, restrictedToMinimumLevel, batchPostingLimit, period, formatProvider, mailSubject);
+        public static LoggerConfiguration Email(this LoggerSinkConfiguration loggerConfiguration, MyEmailConnectionInfo myConnectionInfo, ITextFormatter textFormatter, LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose, int batchPostingLimit = 100, TimeSpan? period = null, string mailSubject = "Log Email") =>
+            loggerConfiguration.Email((EmailConnectionInfo)myConnectionInfo, textFormatter, restrictedToMinimumLevel, batchPostingLimit, period, mailSubject);
+
+        public class MyEmailConnectionInfo : EmailConnectionInfo
+        {
+            public new NetworkCredential NetworkCredentials
+            {
+                get => (NetworkCredential)base.NetworkCredentials;
+                set => base.NetworkCredentials = value;
+            }
+        }
+    }
+}
